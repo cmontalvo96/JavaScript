02331dac82429677a0de358360b1b1219d46ed75
Carlos Montalvo <cmontalvo96@Carloss-MacBook-Pro.local>
2020-09-11 20:55:46 -0400

    initial commit

---
+++ Script.js
@@ -0,0 +1,17 @@
+var firstName = "Carlos";
+var lastName = "Montalvo";
+alert("My name is " + firstName + " " + lastName)
+
+var age = 23
+alert ("I am " + age + "years old.");
+
+if (age < 21) {
+    alert("It is illegal for me to drink in the states.");
+} else if (age > 21) {
+    alert("I am legally able to drink in the states.");
+} else {
+    alert("I am just barely able to drink legally in the states.");
+}
+    
+var coding = " I enjoy this class as I am learning about web development and coding";
+console.log(coding);
